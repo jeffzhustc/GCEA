06eabc01270698352bed1e1c82f33a73e49b3c8f
jeffzhustc <jeffzhustc@gmail.com>
2019-07-14 21:59:36 +0800

    update code
    updating

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import rdkit.Chem as Chem
 
 class Config(object):
     def __init__(self):
+        '''hyperparameters for Molecule class'''
         self.possible_bonds = [
             Chem.rdchem.BondType.SINGLE,
             Chem.rdchem.BondType.DOUBLE,
@@ -21,3 +22,7 @@ class Config(object):
         self.vocab_nodes_encode = {
             'C': 1, 'N': 2, 'O': 3, 'S': 3, 'F': 4, 'Cl': 4, 'Br': 4, 'I': 4
         }
+        '''hyperparameters for Population class'''
+        self.poplution_size = 500
+        self.crossover_rate = 0.8
+        self.init_poplution_file_name = 'data/init_population.sdf'
--- population.py
+++ population.py
@@ -1,3 +1,21 @@
+'''this file is to design a Population class for evolutionary algorithm'''
+
+from config import *
+from molecule import Molecule
+
+config = Config()
+
 class Population(object):
-    def __init__(self):
-        
+    def __init__(self, ):
+        self.population_pool = []
+        self.population_size = config.poplution_size
+        self.init_poplution_file_name = config.init_poplution_file_name
+        self.crossover_rate = config.crossover_rate
+
+        self._init_population()
+
+    def _init_population(self):
+        init_data = Chem.SDMolSupplier(self.init_poplution_file_name)
+        for i in range(self.population_size):
+            self.population_pool.append(Molecule(Chem.MolToSmiles(init_data[i])))
+

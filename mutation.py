b9cf10049d7afb3e6effa2819eb3a2e4f94fd53b
jeffzhustc <jeffzhustc@gmail.com>
2019-07-15 10:50:15 +0800

    update code
    updating

--- mutation.py
+++ mutation.py
@@ -33,3 +33,39 @@ def _add_bond(molecule):
         return Molecule(goal_smiles)
     else:
         return molecule
+
+def _add_atom_between_bond(molecule):
+    temp_elements = {
+        0: 'C',
+        1: 'N',
+        2: 'O',
+        3: 'S'
+    }
+    atom_index = np.random.choice(4, 1)[0]
+    atom = temp_elements[atom_index]
+
+    temp_adj = molecule.adj
+
+    length = molecule.num_atom
+    insert_index1 = np.random.choice(length, 1)
+    insert_row = temp_adj[insert_index1][0]
+
+    for i in range(len(insert_row)):
+        if insert_row[i] > 0:
+            insert_index2 = i
+
+    temp_adj[insert_index1, insert_index2] = temp_adj[insert_index2, insert_index1] = 0
+
+    goal_adj = np.zeros([length+1, length+1])
+    goal_adj[:length, :length] = temp_adj
+    goal_adj[length, insert_index1] = goal_adj[insert_index1, length] = 1
+    goal_adj[insert_index2, length] = goal_adj[length, insert_index2] = 1
+
+    temp_node_list = molecule.node_list
+    temp_node_list.append(atom)
+    goal_node_list = temp_node_list
+
+    goal_mol = adj2mol(goal_node_list, goal_adj.astype(int), deconfig.possible_bonds)
+    goal_smiles = Chem.MolToSmiles(goal_mol)
+
+    return Molecule(goal_smiles)
